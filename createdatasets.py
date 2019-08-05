5eb00690d5fa097dda646e31e85d7942c9b9ddce
David Mason <djmot@users.noreply.github.com>
2019-08-04 21:24:02 -0400

    add pulsar example

--- createdatasets.py
+++ createdatasets.py
@@ -3,6 +3,8 @@
     
     Contains:
       createDatasetBC -- breast cancer dataset via sklearn
+      createDatasetHTRU -- pulsar dataset 
+        (https://archive.ics.uci.edu/ml/datasets/HTRU2#)
 """
 
 
@@ -12,6 +14,7 @@ import pandas as pd
 
 from sklearn.preprocessing import StandardScaler
 from sklearn.datasets import load_breast_cancer
+from sklearn.utils import shuffle
 
 import logging
 
@@ -51,6 +54,46 @@ def createDatasetBC(trainFilename, testFilename, otherFilename, testFraction):
             
     logging.debug('Scaling...')
     features = [c for c in df.columns if c != 'target']
+    scaler = StandardScaler().fit(train[features])
+    train[features] = scaler.transform(train[features])
+    test[features] = scaler.transform(test[features])
+    
+    logging.debug('Saving...')
+    train.to_csv(trainFilename, index=False)
+    test.to_csv(testFilename, index=False)
+    pickle.dump({'scaler': scaler}, open(otherFilename, 'wb'))
+    logging.debug('  Done')
+    
+    
+def createDatasetHTRU2(trainFilename, testFilename, otherFilename, positiveFraction):
+    """ Load the HTRU2 dataset and prepare for model fitting.
+    
+        Arguments:
+        
+        trainFilename -- .csv filename for train set
+        testFilename  -- .csv filename for test set
+        otherFilename -- .pkl filename for dict of containing scaler
+        positiveFraction -- fraction of positive class in train data
+            (e.g. if .25, then train will be 25% positive calss, 75% negative class)
+    """
+    
+    logging.debug('Loading...')
+    data = pd.read_csv('HTRU_2.csv', header=None)
+    data.columns = ['feature_' + str(c) for c in data.columns[:-1]] + ['target']
+    
+    logging.debug('Resampling train/test sets...')
+    data_0 = shuffle(data.loc[data.target == 0], random_state = 314159)
+    data_1 = shuffle(data.loc[data.target == 1], random_state = 314159)
+    data_1_test  = data_1.iloc[:250]
+    data_1_train = data_1.iloc[250:]
+    class_0_train_length = int(len(data_1_train)*(1/positiveFraction - 1))
+    data_0_train = data_0.iloc[:class_0_train_length]
+    data_0_test  = data_0.iloc[class_0_train_length:]
+    train = shuffle(pd.concat((data_0_train, data_1_train), axis=0))
+    test  = shuffle(pd.concat((data_0_test, data_1_test), axis=0))
+            
+    logging.debug('Scaling...')
+    features = [c for c in train.columns if c != 'target']
     scaler = StandardScaler().fit(train[features])
     train[features] = scaler.transform(train[features])
     test[features] = scaler.transform(test[features])
---
+++ pulsar_example - Copy.py
@@ -0,0 +1,82 @@
+"""
+    Compare stacked model to basic models on HTRU2 pulsar dataset
+"""
+
+
+import pickle
+import numpy as np
+import pandas as pd
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+
+from createdatasets import createDatasetHTRU2
+from stackedmodel import Data, Stacked, featureImportances
+
+
+TRAIN_FILENAME = 'htru2_train_data.csv'
+TEST_FILENAME = 'htru2_test_data.csv'
+OTHER_FILENAME = 'htru2_other.pkl'
+
+""" Uncomment if you need to recreate the dataset
+"""
+#createDatasetHTRU2(TRAIN_FILENAME, TEST_FILENAME, OTHER_FILENAME, 0.2)
+"""
+"""
+
+data = Data(TRAIN_FILENAME, TEST_FILENAME, OTHER_FILENAME)
+X_train, y_train, X_test, y_test, scaler = data.load()
+features = data.features
+data.inspectData()
+
+baseModels = {
+    'l1_regression': 
+        LogisticRegression(
+            penalty = 'l1',
+            C = 0.5,
+            solver = 'liblinear',
+            max_iter = 100,
+        ),
+    'l2_regression': 
+        LogisticRegression(
+            penalty = 'l2',
+            C = 0.5,
+            solver = 'liblinear',
+            max_iter = 100,
+        ),
+}
+
+for depth in (4, 12, 32):
+    key = 'tree_depth_' + str(depth)
+    baseModels[key] = DecisionTreeClassifier(
+        max_depth = depth,
+        min_samples_leaf = 5,
+    )
+
+for k in (1, 2, 4, 8, 16, 32):
+    key = str(k) + '_nn'
+    baseModels[key] = KNeighborsClassifier(n_neighbors = k)
+
+metaModel = RandomForestClassifier(
+    max_depth = 12,
+    min_samples_leaf = 5,
+    n_estimators = 200,
+    random_state = 314159,
+)
+
+stackedModel = Stacked(data, baseModels, metaModel=metaModel)
+
+print('Fitting models...')
+stackedModel.fitAllModels()
+print('Scoring...')
+stackedModel.scoreAllModels(train=False, test=True, verbose=False)
+print('  Done')
+
+print('\nMeta model feature importances (top 20):')
+print(featureImportances(stackedModel.metaModel, stackedModel.features)[:20])
+
+print('\nResults:')
+print(stackedModel.scoreDataFrame())
+
